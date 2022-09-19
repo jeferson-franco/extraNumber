428943f84a616278b0810e334fa17346b1e16d84
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-19 08:07:17 -0300

    feat: create extraNumber.js

---
+++ src/extraNumber.js
@@ -0,0 +1,12 @@
+function solution(a, b, c) {
+    return a ^ b ^ c;
+}
+
+module.exports = solution;
+
+// npm init -y
+// npm i jest-cli -g
+// package.json: "scripts": { "test": "jest" }
+// npm test extraNumber
+
+// alternative solution
